fe70b9c2f2c52827c7119be3845e788af193e3f5
selvakumar <selvakumar_rajendren@goaaa.com>
2012-11-11 10:25:54 -0700

    initial version

---
+++ bonfire/application/views/home/index.php
@@ -0,0 +1,78 @@
+
+<div class="container">
+
+
+	<div class="hero-unit">
+		<h1>Tiny Habit!</h1>
+		<h5> Simple tool to create good habits..</h5>
+		<p>When you know how to create tiny habits, you can change your lifeforever.</p>
+		<p><?php 
+		echo anchor('login/detailsPage', 'Learn more >>','class="btn btn-primary btn-large"');
+		?></p>
+	</div>
+
+	<div class="row">
+
+
+	
+
+			<div class="span4">
+				<h2>What is a "Tiny Habit"?</h2>
+				<p>
+					<ol>
+							<li>
+								<h6>you do at least once a day
+								</h4>
+							
+							</li>
+							<li>
+								<h6>that takes you less then 30 seconds
+							</h4>
+							
+							</li>
+							<li>
+								<h6>that requires little effort
+							</h4>
+							</li>
+
+					</ol>
+
+				</p>
+					<p><a href="#" class="btn btn-primary btn-large">View details >></a></p>
+			</div>
+			
+			<div class="span4">
+				<h2>Weekly Habits!</h2>
+				<p>
+				<ol>
+					<li>
+						<h6>After I brush, I will floss one tooth
+						</h6>
+					</li>
+					<li>
+						<h6>After I arrive home from work, I will hang up my keys on the hook
+						by the door
+						</h6>
+	
+					</li>
+					<li>
+						<h6>After I pour coffee in the morning, I will wipe one kitchen counter
+						</h6>
+	
+					</li>
+
+
+</ol>
+</p>
+<p><a href="#" class="btn btn-primary btn-large">View details >></a></p>
+</div>
+
+</div>
+
+
+
+
+
+
+
+</div>
